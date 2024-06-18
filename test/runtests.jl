866d5ec28464750fc9be9be19db3037f417199a9
bmergl <bmergl@outlook.de>
2024-06-18 15:22:42 +0200

    Add tests for 5D/6D SauterSchwab cases

---
+++ test/runtests.jl
@@ -0,0 +1,25 @@
+using StaticArrays
+using LinearAlgebra
+using CompScienceMeshes
+using Combinatorics
+using SauterSchwab3D
+
+using Test
+
+
+
+include("test_cv_3d.jl")
+include("test_ce_3d.jl") # Simplex Product not fixed! (CommonEdge6D_S - Domain 5)
+include("test_cf_3d.jl")
+include("test_ct_3d.jl")
+
+
+include("test_cv_2.5d.jl")
+include("test_ce_2.5d.jl")
+include("test_cf_2.5d.jl")
+
+
+# include("test_cv_2d.jl")
+# include("test_ce_2d.jl")
+# include("test_cf_2d.jl")
+
---
+++ test/test_cv_3d.jl
@@ -0,0 +1,137 @@
+using LinearAlgebra
+using CompScienceMeshes
+using SauterSchwab3D
+using StaticArrays
+using Combinatorics
+using Test
+
+include("$(pkgdir(SauterSchwab3D))/example/singularity_detection.jl")
+
+function is_CSM_tet(s)
+   CSM_tet = false
+
+   c = cross(s.tangents[1], s.tangents[2])
+   dot(c,s.tangents[3]) > 0.0 && (CSM_tet = true)
+
+   return CSM_tet
+end
+
+
+
+pI   = point(0,0,0)
+pII  = point(1,0,0)
+pIII = point(0,1,0)
+pIV  = point(0,0,1)
+
+qII  = point(-1,0,0)
+qIII = point(0,-1,0)
+qIV  = point(0,0,-1)
+
+
+P_ = simplex(pI,pII,pIV,pIII)
+Q_ = simplex(pI,qII,qIII,qIV)
+
+@test is_CSM_tet(P_) == true
+@test is_CSM_tet(Q_) == true
+
+sing_ = singularity_detection(P_,Q_)
+cv_ref = CommonVertex6D(sing_,SauterSchwab3D._legendre(10,0.0,1.0))
+
+
+# Tensor Product: Kernel=1 Test
+function integrand1(x,y)
+   return 1.0
+end
+function INTEGRAND1(u,v)
+   n1 = neighborhood(P_,u)
+   n2 = neighborhood(Q_,v)
+   x = cartesian(n1)
+   y = cartesian(n2)
+   output = integrand1(x,y)*jacobian(n1)*jacobian(n2)
+   return(output)
+end
+num1 = sauterschwab_parameterized(INTEGRAND1, cv_ref)
+exact1 = Q_.volume*P_.volume
+err1 = abs(num1-exact1)/abs(exact1)
+@test err1 < 1.0e-12
+
+
+# Tensor Product: Regular Kernel Test
+function integrand2(x,y)
+   return 1 +
+   2*x[1]-3*x[2]+4*x[3]-5*y[1]+6*y[2]-7*y[3] +
+   2*x[1]^2-3*x[2]^3+4*x[3]^4-5*y[1]^5+6*y[2]^6-7*y[3]^7 +
+   y[1]*y[2]*y[3]*x[1]*x[2]*x[3]
+end
+function INTEGRAND2(u,v)
+   n1 = neighborhood(P_,u)
+   n2 = neighborhood(Q_,v)
+   x = cartesian(n1)
+   y = cartesian(n2)
+   output = integrand2(x,y)*jacobian(n1)*jacobian(n2)
+   return(output)
+end
+num2 = sauterschwab_parameterized(INTEGRAND2, cv_ref)
+exact2 = 52319/518400
+err2 = abs(num2-exact2)/abs(exact2)
+@test err2 < 1.0e-12
+
+
+# Tensor Product: Singular Kernel Reference (for comparison with Simplex Product below)
+function integrand(x,y)
+   return ((x-pI)'*(y-qIII))*(exp(-im*1*norm(x-y))/(4pi*norm(x-y)))
+end
+function INTEGRAND(u,v)
+   n1 = neighborhood(P_,u)
+   n2 = neighborhood(Q_,v)
+   x = cartesian(n1)
+   y = cartesian(n2)
+   output = integrand(x,y)*jacobian(n1)*jacobian(n2)
+   return(output)
+end
+ref = sauterschwab_parameterized(INTEGRAND, cv_ref)
+
+
+
+
+## Simplex Product vs. Tensor Product reference for all permutations
+list_tvert = collect(permutations([pI,pII,pIII,pIV]))
+list_svert = collect(permutations([pI,qII,qIII,qIV]))
+
+for tvert in list_tvert
+   for svert in list_svert
+      P = simplex(tvert[1],tvert[2],tvert[3],tvert[4])
+      Q = simplex(svert[1],svert[2],svert[3],svert[4])
+      
+      sing = singularity_detection(P,Q)
+      I,J = reorder(sing)
+
+      P_new = simplex(P.vertices[I])
+      Q_new = simplex(Q.vertices[J])
+
+      # expected reorder result:
+      # Pt = [P, A1, A2, A3]
+      # Ps = [P, B1, B2, B3]
+      @test norm(P_new[1] - Q_new[1]) < 1.0e-14
+
+      if is_CSM_tet(P) == true && is_CSM_tet(Q) == true
+         @test is_CSM_tet(P_new) == true
+         @test is_CSM_tet(Q_new) == true
+         
+         function INTEGRAND(u,v)
+            n1 = neighborhood(P_new,u)
+            n2 = neighborhood(Q_new,v)
+            x = cartesian(n1)
+            y = cartesian(n2)
+            output = integrand(x,y)*jacobian(n1)*jacobian(n2)
+            return(output)
+         end
+         cv_s = CommonVertex6D_S(sing,SauterSchwab3D._shunnham3D(6))
+         int_sp = sauterschwab_parameterized(INTEGRAND, cv_s)
+
+         err_sp = norm(int_sp-ref)/norm(ref)
+         @test err_sp < 5.0e-6
+      end
+   end
+end
+print("CommonVertex6D_S test finished")
